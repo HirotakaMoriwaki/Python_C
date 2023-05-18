32ab17a1468f6f888143730b8b7b89b718b74716
RioNaito0205 <naito.rio.jm@mynavi.jp>
2023-05-18 08:09:08 +0000

    add_

--- Naito/subject.py
+++ Naito/subject.py
@@ -12,13 +12,11 @@ sam=ma_p+ja_p+en_p
 #判定フラグ
 hantei = False
 
-if((ma_p >= 70 & ja_p >=70 & en_p >=70) or sam >=220):
-    hantei = True
-    if(ma_p < 50 or ja_p < 50 or en_p < 50):
-        hantei=False
-elif(ma_p < 50 or ja_p < 50 or en_p < 50):
+if(ma_p < 50 or ja_p < 50 or en_p < 50):
     hantei = False
-    
+elif((ma_p >= 70 & ja_p >=70 & en_p >=70) or sam >=220):
+    hantei = True
+
 if hantei:
     print("合格",end="")
 else:
