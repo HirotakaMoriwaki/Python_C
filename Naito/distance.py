a7a21d9c27a2da0a1cb8b412123c2c4e06b77742
RioNaito0205 <naito.rio.jm@mynavi.jp>
2023-05-19 05:56:15 +0000

    add_4

---
+++ Naito/distance.py
@@ -0,0 +1,19 @@
+#東京駅からの距離を求めるプログラム
+import sys 
+
+args =sys.argv
+
+start = args[1]
+end = args[2]
+
+dis ={
+    "東京":0.00,
+    "品川":6.78,
+    "新横浜":25.54,
+    "名古屋":342.02,
+    "京都":476.31,
+    "新大阪":515.35
+}
+print('{:.2f}'.format(abs(dis[end]-dis[start])),end='')
+
+
