70453b948ccf9266964868e23202b6260080eed6
RioNaito0205 <naito.rio.jm@mynavi.jp>
2023-05-19 05:14:47 +0000

    add_3

--- Naito/animallist.py
+++ Naito/animallist.py
@@ -3,20 +3,20 @@ import sys
 
 args = sys.argv
 #変数に引数を代入
-#場所
+#場所/動物名
 k = int(args[1])
-#動物名
 name = args[2]
-if(k < 0 or k > 5):
-    print("0から5の整数値を入力してください")
+
 #動物のリスト
 animal = ["giraffe", "tiger", "zebra", "elephant", "lion"]
 #挿入
 animal.insert(k,name)
 
 #最後の要素削除
-del animal[len(animal)-1]
+#del animal[len(animal)-1]#lenは１から
+del animal[-1]
 
 #リストを昇順に並べる
 animal.sort(reverse=False)
+
 print(animal,end="")
