48866c4ec40a9ec33a6050191c31cabf254a89e1
RioTakahashi-6 <takahashi.rio.pw@mynavi.jp>
2023-05-19 08:18:14 +0000

    お疲れさまでした！！！

---
+++ Takahashi/distanceerror.py
@@ -0,0 +1,20 @@
+import sys
+args = sys.argv
+
+distance = {'東京':0.00, '品川':6.78, '新横浜':25.54, '名古屋':342.02, '京都':476.31, '新大阪':515.35}
+sta1 = args[1]
+sta2 = args[2]
+
+try:
+    if distance[sta1] > distance[sta2]:
+        d = (distance[args[2]] - distance[args[1]]) * -1
+        print('{:.2f}'.format(d),end="")
+    else:
+        d = distance[args[2]] - distance[args[1]]
+        print('{:.2f}'.format(d),end="")
+
+except:
+    print("のぞみの停車駅を引数に設定してください",end="")
+
+    
+    
