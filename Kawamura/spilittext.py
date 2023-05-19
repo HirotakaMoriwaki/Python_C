89ee777ef44449f430c6dbf42962cd26e5b39dbc
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-05-19 06:49:36 +0000

    コメント追記

--- Kawamura/spilittext.py
+++ Kawamura/spilittext.py
@@ -4,6 +4,6 @@ args = sys.argv
 input1 = args[1] 
 input2 = args[2]
 
-words = input1.split()
+words = input1.split() # 空白で区切る
 
-print(words[int(input2)-1], end="")
+print(words[int(input2)-1], end="") # 何番目の文字を指定する
