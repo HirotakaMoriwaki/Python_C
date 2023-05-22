76e106749343d9d3861aa47981229f7a2bd4d5d2
HirotakaMoriwaki <moriwaki.hirotaka.jm@mynavi.jp>
2023-05-22 07:27:28 +0000

    myfamily_Moriwaki訂正

--- Moriwaki/myfamily_Moriwaki.py
+++ Moriwaki/myfamily_Moriwaki.py
@@ -4,7 +4,6 @@ import sys
 list_Family = sys.argv[1:]
 myFam = IntroFam(list_Family[0],list_Family[1],list_Family[2])
 
-print(list_Family)
 print(myFam.name_out())
 print(myFam.age_out())
 print(myFam.cat_out())
