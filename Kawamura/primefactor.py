51369879403668beb1048f474be3585840c61cfa
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-05-22 05:22:32 +0000

    素因数分解と生徒の点数平均

--- Kawamura/primefactor.py
+++ Kawamura/primefactor.py
@@ -1,4 +1,17 @@
 import sys
 args = sys.argv
 
-input1 = args[1]
+input1 = int(args[1])
+list_1 = []
+
+i = 2
+
+while i != input1:
+    if input1 % i == 0:
+        input1 = input1 / i
+        list_1.append(i)
+    if input1 % i != 0:
+        i = i + 1
+
+list_1.append(i)       
+print(list_1)
