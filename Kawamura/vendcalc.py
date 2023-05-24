dc56cbba00589b9efb30f581b517080c5103bec6
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-05-24 00:40:05 +0000

    自販機問題int→str

--- Kawamura/vendcalc.py
+++ Kawamura/vendcalc.py
@@ -14,22 +14,22 @@ def oturi(remain_money):
         remain_money = remain_money - 5000
         print("5000円：1枚")
     if remain_money > 2000:
-        print("2000円：" + Decimal(remain_money / 2000).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN) + "枚") 
+        print("2000円：" + str(Decimal(remain_money / 2000).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN)) + "枚") 
         remain_money = remain_money % 2000
     if remain_money > 1000:
-        print("1000円：" + Decimal(remain_money / 1000).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN) + "枚") 
+        print("1000円：" + str(Decimal(remain_money / 1000).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN)) + "枚") 
         remain_money = remain_money % 1000
     if remain_money > 500:
-        print("500円：" + Decimal(remain_money / 500).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN) + "枚") 
+        print("500円：" + str(Decimal(remain_money / 500).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN)) + "枚") 
         remain_money = remain_money % 500
     if remain_money > 100:
-        print("100円：" + Decimal(remain_money / 100).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN) + "枚") 
+        print("100円：" + str(Decimal(remain_money / 100).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN)) + "枚") 
         remain_money = remain_money % 100
     if remain_money > 50:
-        print("50円：" + Decimal(remain_money / 50).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN) + "枚") 
+        print("50円：" + str(Decimal(remain_money / 50).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN)) + "枚") 
         remain_money = remain_money % 50
     if remain_money > 10:
-        print("10円：" + Decimal(remain_money / 10).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN) + "枚") 
+        print("10円：" + str(Decimal(remain_money / 10).quantize(Decimal("0"),rounding=ROUND_HALF_DOWN)) + "枚") 
         remain_money = remain_money % 10
 
 while True:
