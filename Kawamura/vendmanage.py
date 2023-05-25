fbba1fe33f4a2da6183f775cc25b22f1b559e5e5
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-05-25 05:18:07 +0000

    自販機

--- Kawamura/vendmanage.py
+++ Kawamura/vendmanage.py
@@ -3,23 +3,23 @@ from sqlalchemy import Column, String, Integer, DateTime
 from database import Base
 from database import ENGINE
 
-class merchandise(Base):
+class Merchandise(Base):
     __tablename__ = 'mst_merchandise'
     id = Column('id', String(10), primary_key = True)
     name = Column('name',String(20))
     price = Column('price',Integer)
 
-class stock(Base):
+class Stock(Base):
     __tablename__ = 'tbl_stock'
     id = Column('id', String(10), primary_key = True)
     stock = Column('stock',Integer)
 
-class money(Base):
+class Money(Base):
     __tablename__ = 'tbl_money'
     price = Column('price', Integer, primary_key = True)
     number = Column('number',Integer)
 
-class message(Base):
+class Message(Base):
     __tablename__ = 'tbl_message'
     seq= Column('seq', Integer, primary_key = True)
     message = Column('message', String(100))
