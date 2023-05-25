83fb9b4aec62cababbe65f8e1d5418472c0538c7
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-05-25 05:16:48 +0000

    インサート

---
+++ MySQLTraining.session.sql
@@ -0,0 +1,27 @@
+INSERT INTO mst_merchandise(id, name, price) VALUES
+    ("1000000000","お茶",110),
+    ("1000000001","コーヒー",100),
+    ("1000000002","ソーダ",160),
+    ("1000000003","コーンポタージュ",130)
+
+INSERT INTO tbl_stock(id, stock) VALUES
+    ("1000000000",10),
+    ("1000000001",50),
+    ("1000000002",60),
+    ("1000000003",30)
+
+INSERT INTO tbl_money(price, number) VALUES
+    (5000, 300),
+    (2000,300),
+    (1000,300),
+    (500, 300),
+    (100, 300),
+    (50, 300),
+    (10, 300)
+
+INSERT INTO tbl_message(seq, message, datetime) VALUES
+    (5000, 300),
+    (2000,300),
+    (1000,300),
+    (500, 300)
+
