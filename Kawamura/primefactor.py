c31a5e0436590b7acdc893c1568d3bfc9b53aa56
kentarokawamura1 <kawamura.kentaro.gq@mynavi.jp>
2023-05-25 05:17:45 +0000

    素数

--- Kawamura/primefactor.py
+++ Kawamura/primefactor.py
@@ -8,8 +8,8 @@ i = 2
 
 while i != input1:
     if input1 % i == 0:
-        input1 = input1 / i
-        list_1.append(i)
+        input1 = input1 // i
+        list_1.append(i) 
     if input1 % i != 0:
         i = i + 1
 
